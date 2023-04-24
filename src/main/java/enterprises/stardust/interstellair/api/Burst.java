6668cd9bd5b250f72dfdcd47a37811761c554258
xtrm <oss@xtrm.me>
2023-04-24 15:26:48 +0200

    ✨ feature(bursts): added more Stream API support + implements BaseStream

--- src/main/java/enterprises/stardust/interstellair/api/Burst.java
+++ src/main/java/enterprises/stardust/interstellair/api/Burst.java
@@ -1,10 +1,14 @@
 package enterprises.stardust.interstellair.api;
 
 import enterprises.stardust.interstellair.impl.InterstellairBurst;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.function.*;
+import java.util.stream.BaseStream;
 import java.util.stream.Collector;
 
 /**
@@ -13,20 +17,72 @@ import java.util.stream.Collector;
  * @version 1.0
  */
 @SuppressWarnings("unchecked")
-public abstract class Burst<T> {
-    public abstract <N> Burst<N> map(Function<T, N> mapper);
+public abstract class Burst<T> implements BaseStream<T, Burst<T>>, Iterable<T> {
+    public abstract <N> Burst<N> map(Function<? super T, ? extends N> mapper);
 
-    public abstract Burst<T> filter(Predicate<T> filter);
+    public abstract Burst<T> filter(Predicate<? super T> filter);
 
-    public abstract T[] toArray();
+    public abstract Burst<T> peek(Consumer<? super T> action);
+
+    public abstract void forEach(Consumer<? super T> action);
 
     public abstract long count();
 
     public abstract <C extends Collection<? super T>> C collect(C collection);
 
+    public abstract <R, A> R collect(Collector<? super T, A, R> collector);
+
+    public abstract <R> R collect(Supplier<R> supplier, BiConsumer<R, ? super T> accumulator, BiConsumer<R, R> combiner);
+
     public abstract <C extends Collection<T>> C collect(C... reified);
 
-//    public abstract <C extends Collection<T>> C collect(Collector<T, ?, C> collector);
+    public abstract T[] toArray();
+
+    public abstract Optional<T> first();
+
+    public abstract Optional<T> last();
+
+    public Optional<T> findFirst() {
+        return first();
+    }
+
+    @NotNull
+    @Override
+    public Burst<T> sequential() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @NotNull
+    @Override
+    public Burst<T> parallel() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @NotNull
+    @Override
+    public Burst<T> unordered() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public boolean isParallel() {
+        return false; //TODO
+    }
+
+    @NotNull
+    @Override
+    public abstract Burst<T> onClose(Runnable closeHandler);
+
+    @Override
+    public abstract void close();
+
+    @NotNull
+    @Override
+    public abstract Iterator<T> iterator();
+
+    @NotNull
+    @Override
+    public abstract Spliterator<T> spliterator();
 
     public static <T> Burst<T> ofArray(T[] objects) {
         return new InterstellairBurst<>(objects);
--- src/main/java/enterprises/stardust/interstellair/impl/InterstellairBurst.java
+++ src/main/java/enterprises/stardust/interstellair/impl/InterstellairBurst.java
@@ -1,12 +1,12 @@
 package enterprises.stardust.interstellair.impl;
 
 import enterprises.stardust.interstellair.api.Burst;
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Array;
 import java.util.*;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
+import java.util.stream.Collector;
 
 /**
  * @param <T> the objects type
@@ -15,20 +15,34 @@ import java.util.function.Supplier;
 @SuppressWarnings({"unchecked", "ForLoopReplaceableByForEach"})
 public class InterstellairBurst<T> extends Burst<T> {
     private static final Map<Class<?>, Supplier<Collection<?>>>
-            ALLOCATION_TABLE = new HashMap<>();
+        ALLOCATION_TABLE = new HashMap<>();
+    public static final Object NULL_HOLDER = new Object();
+    private final Class<?> type;
     private final Object[] objects;
+    private final List<Runnable> closeRunnables = new ArrayList<>();
     private boolean used;
 
     public InterstellairBurst(T[] objects) {
-        this.objects = objects;
+        this.type = objects.getClass().getComponentType();
+        if (type.isPrimitive()) {
+            throw new IllegalArgumentException("Primitive arrays are not supported yet");
+        } else {
+            if (type == Object.class) {
+                this.objects = objects;
+            } else {
+                // recontextualize
+                this.objects = new Object[objects.length];
+                System.arraycopy(objects, 0, this.objects, 0, objects.length);
+            }
+        }
     }
 
     @Override
-    public <N> Burst<N> map(Function<T, N> mapper) {
+    public <N> Burst<N> map(Function<? super T, ? extends N> mapper) {
         for (int i = 0; i < objects.length; i++) {
             Object object = objects[i];
-            if (object == null) {
-                objects[i] = null;
+            if (object == NULL_HOLDER) {
+                objects[i] = NULL_HOLDER;
             } else {
                 objects[i] = mapper.apply((T) object);
             }
@@ -39,37 +53,38 @@ public class InterstellairBurst<T> extends Burst<T> {
     }
 
     @Override
-    public Burst<T> filter(Predicate<T> filter) {
+    public Burst<T> filter(Predicate<? super T> filter) {
         for (int i = 0; i < objects.length; i++) {
             Object object = objects[i];
-            if (object == null) continue;
+            if (object == NULL_HOLDER) continue;
             if (!filter.test((T) object)) {
-                objects[i] = null;
+                objects[i] = NULL_HOLDER;
             }
         }
         return this;
     }
 
     @Override
-    public T[] toArray() {
-        terminalOperation();
-
-        //TODO: find out if reallocating the array is faster than copying
-        //existing elements inside #objects
-        long newArrayLength = internalCount();
-        if (newArrayLength > Integer.MAX_VALUE) {
-            throw new IllegalStateException("Array length is too big");
-        }
-        T[] array = (T[])
-                Array.newInstance(objects.getClass().getComponentType(), (int) newArrayLength);
-        int i = 0;
-        for (int j = 0; j < objects.length; j++) {
-            Object object = objects[j];
-            if (object != null) {
-                array[i++] = (T) object;
+    public Burst<T> peek(Consumer<? super T> action) {
+        for (int i = 0; i < objects.length; i++) {
+            Object object = objects[i];
+            if (object != NULL_HOLDER) {
+                action.accept((T) object);
             }
         }
-        return array;
+        return this;
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        terminalOperation();
+
+        for (int i = 0; i < objects.length; i++) {
+            Object object = objects[i];
+            if (object != NULL_HOLDER) {
+                action.accept((T) object);
+            }
+        }
     }
 
     @Override
@@ -82,11 +97,32 @@ public class InterstellairBurst<T> extends Burst<T> {
     private long internalCount() {
         long count = 0L;
         for (int i = 0; i < objects.length; i++) {
-            if (objects[i] != null) {
+            if (objects[i] != NULL_HOLDER) {
                 count++;
             }
         }
         return count;
+    }
+
+    @Override
+    public T[] toArray() {
+        terminalOperation();
+
+        //TODO: find out if reallocating the array is faster than copying
+        //existing elements inside #objects
+        long newArrayLength = internalCount();
+        if (newArrayLength > Integer.MAX_VALUE) {
+            throw new IllegalStateException("Array length is too big");
+        }
+        T[] array = (T[]) Array.newInstance(type, (int) newArrayLength);
+        int i = 0;
+        for (int j = 0; j < objects.length; j++) {
+            Object object = objects[j];
+            if (object != NULL_HOLDER) {
+                array[i++] = (T) object;
+            }
+        }
+        return array;
     }
 
     @Override
@@ -95,7 +131,7 @@ public class InterstellairBurst<T> extends Burst<T> {
 
         for (int i = 0; i < objects.length; i++) {
             Object object = objects[i];
-            if (object != null) {
+            if (object != NULL_HOLDER) {
                 collection.add((T) object);
             }
         }
@@ -108,15 +144,98 @@ public class InterstellairBurst<T> extends Burst<T> {
         terminalOperation();
 
         Class<? extends Collection<T>> clazz = (Class<? extends Collection<T>>)
-                reified.getClass().getComponentType();
+            reified.getClass().getComponentType();
         Supplier<? extends Collection<?>> supplier =
-                ALLOCATION_TABLE.getOrDefault(clazz, null);
+            ALLOCATION_TABLE.getOrDefault(clazz, null);
         if (supplier == null) {
             throw new IllegalArgumentException("No supplier found for class " +
-                    clazz);
+                clazz);
         }
         C collection = (C) supplier.get();
         return collect(collection);
+    }
+
+    @Override
+    public <R, A> R collect(Collector<? super T, A, R> collector) {
+        terminalOperation();
+
+        A container = collector.supplier().get();
+        BiConsumer<A, ? super T> accumulator = collector.accumulator();
+        for (int i = 0; i < objects.length; i++) {
+            Object object = objects[i];
+            if (object != NULL_HOLDER) {
+                accumulator.accept(container, (T) object);
+            }
+        }
+        return collector.finisher().apply(container);
+    }
+
+    @Override
+    public <R> R collect(Supplier<R> supplier, BiConsumer<R, ? super T> accumulator, BiConsumer<R, R> combiner) {
+        terminalOperation();
+
+        R container = supplier.get();
+        for (int i = 0; i < objects.length; i++) {
+            Object object = objects[i];
+            if (object != NULL_HOLDER) {
+                accumulator.accept(container, (T) object);
+            }
+        }
+        return container;
+    }
+
+    @Override
+    public Optional<T> first() {
+        terminalOperation();
+
+        for (int i = 0; i < objects.length; i++) {
+            Object object = objects[i];
+            if (object != NULL_HOLDER) {
+                return Optional.of((T) object);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<T> last() {
+        terminalOperation();
+
+        Object last = null;
+        for (int i = 0; i < objects.length; i++) {
+            Object object = objects[i];
+            if (object != NULL_HOLDER) {
+                last = object;
+            }
+        }
+        return Optional.ofNullable((T) last);
+    }
+
+    @NotNull
+    @Override
+    public Iterator<T> iterator() {
+        // Terminal operation (by toArray() in BurstIterator)
+        return new BurstIterator();
+    }
+
+    @NotNull
+    @Override
+    public Spliterator<T> spliterator() {
+        // Terminal operation (by toArray in iterator())
+        return Spliterators.spliterator(iterator(), internalCount(),
+            Spliterator.ORDERED | Spliterator.IMMUTABLE);
+    }
+
+    @NotNull
+    @Override
+    public Burst<T> onClose(Runnable closeHandler) {
+        closeRunnables.add(closeHandler);
+        return this;
+    }
+
+    @Override
+    public void close() {
+        terminalOperation();
     }
 
     private void terminalOperation() {
@@ -124,11 +243,28 @@ public class InterstellairBurst<T> extends Burst<T> {
             throw new IllegalStateException("Burst has already been used");
         }
         this.used = true;
+        closeRunnables.forEach(Runnable::run);
     }
 
     static {
         ALLOCATION_TABLE.put(Set.class, HashSet::new);
         ALLOCATION_TABLE.put(List.class, ArrayList::new);
         ALLOCATION_TABLE.put(Queue.class, LinkedList::new);
+    }
+
+    public class BurstIterator implements Iterator<T> {
+        private int index = 0;
+        private final Object[] objects =
+            InterstellairBurst.this.toArray();
+
+        @Override
+        public boolean hasNext() {
+            return index < objects.length;
+        }
+
+        @Override
+        public T next() {
+            return (T) objects[index++];
+        }
     }
 }
